3bfcc1d15d819045f47f7bd510b1aa062ac1ae9d
SunilS <spsarolkar@gmail.com>
2020-02-08 13:32:06 +0530

    updated servername display style on UI and added build script for maven build

--- example/build.sh
+++ example/build.sh
@@ -3,4 +3,5 @@ eval $(minikube docker-env)
 docker rmi spsarolkar/cowsay-deamon:1.0
 docker rmi spsarolkar/fortune-teller:2.0
 (cd cowsay-deamon/cowsay; docker build -t spsarolkar/cowsay-deamon:1.0 .)
+(cd fortune-teller-ui; ./build.sh)
 (cd fortune-teller-ui; docker build -t spsarolkar/fortune-teller:2.0 .)
